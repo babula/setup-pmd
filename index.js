48b0d2536d47748ba3986079fd612dc48b2b8d03
Steve Babula <steve.babula@vicasso.com>
2024-08-22 16:43:30 -0400

    Fix file name

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const core = require('@actions/core')
 const exec = require('child_process').exec
 
+const PMD_VERSION = "7.4.0";
+
 try {
-  installPMD()
+    installPMD()
 } catch (error) {
-  core.setFailed(error.message)
+    core.setFailed(error.message)
 }
 
-function installPMD(){
-  var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.4.0/pmd-dist-7.4.0-bin.zip -P /tmp'
-  var unzip = 'unzip /tmp/pmd-dist-7.4.0-bin.zip -d /tmp'
-  var mk = 'mkdir $HOME/pmd'
-  var mv = 'mv /tmp/pmd-bin-7.4.0/* $HOME/pmd'
-  exec(download+' && '+unzip+' && '+mk+' && '+mv, function(error, stdout, stderr){
-    if(error) core.setFailed(stderr)
-    referencePMD()
-  })
+function installPMD() {
+    var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F${PMD_VERSION}/pmd-dist-${PMD_VERSION}-bin.zip -P /tmp'
+    var unzip = 'unzip /tmp/pmd-dist-${PMD_VERSION}-bin.zip -d /tmp'
+    var mk = 'mkdir $HOME/pmd'
+    var mv = 'mv /tmp/pmd-bin-${PMD_VERSION}/* $HOME/pmd'
+    exec(download + ' && ' + unzip + ' && ' + mk + ' && ' + mv, function(error, stdout, stderr) {
+        if (error) core.setFailed(stderr)
+        referencePMD()
+    })
 }
 
-function referencePMD(){
-  var mk = 'sudo mkdir -p /snap/bin && sudo chmod -R 757 /snap/bin'
-  var cmd = 
-`sudo echo '#! /bin/bash
-$HOME/pmd/bin/run.sh pmd "$@"' > /snap/bin/pmd`
-  var cm = 'chmod +x /snap/bin/pmd'
-  exec(mk+' && '+cmd+' && '+cm, function(error, stdout, stderr){
-    if(error) core.setFailed(stderr)
-    
-  })
+function referencePMD() {
+    var mk = 'sudo mkdir -p /snap/bin && sudo chmod -R 757 /snap/bin'
+    var cmd =
+        `sudo echo '#! /bin/bash
+$HOME/pmd/bin/pmd "$@"' > /snap/bin/pmd`
+    var cm = 'chmod +x /snap/bin/pmd'
+    exec(mk + ' && ' + cmd + ' && ' + cm, function(error, stdout, stderr) {
+        if (error) core.setFailed(stderr)
+    })
 }
